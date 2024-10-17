e84e627c0b7a4339e28ffa484ea3bf799692dd72
jatur <turayjj@mail.uc.edu>
2024-10-17 16:04:59 -0400

    Init Commit

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,5 +1,30 @@
-public class Main {
+import java.util.Scanner;
+public class CtoFConverter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        boolean done = false;
+        double celsius;
+        double fahrenheit;
+        String trash;
+
+        do {
+            System.out.print("Please input a temperature to convert to f: ");
+            if (in.hasNextDouble()){
+
+                celsius = in.nextDouble();
+                System.out.println(celsius);
+                done = true;
+                fahrenheit = (celsius * 9 / 5) + 32;
+
+                System.out.println("your temperature in fahrenheit is: "+fahrenheit);
+
+            }
+            else{
+                trash = in.nextLine();
+                System.out.println("Please enter a valid input, not "+trash);
+
+            }
+        }
+        while(!done);
     }
 }
--- src/HighorLow.java
+++ src/HighorLow.java
@@ -1,4 +1,68 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
+import java.util.Random;
 public class HighorLow {
+    public static void main(String[] args) {
+
+        Random generator = new Random();
+        int targetNum = generator.nextInt(10) + 1;  // val is 1 to 10
+
+        Scanner in = new Scanner(System.in);
+        int guess = 0;
+
+
+        boolean valid = true;
+        String trash = "";
+
+        do{
+            System.out.print("Guess a number between 1-10: ");
+
+            if (in.hasNextInt() ){
+
+                guess = in.nextInt();
+
+                if (guess >= 1 && guess <= 10) {
+
+                    int result = Integer.compare(guess, targetNum); // returns a switch case friendly value
+
+                    switch (result) {
+
+                        case 0:
+
+                            System.out.println("Correct! You guessed on the money!");
+
+                            break;
+
+                        case 1:
+
+                            System.out.println("Number is too high! Try again.");
+
+                            break;
+
+                        case -1:
+
+                            System.out.println("Number is too low! Try again.");
+
+                            break;
+                    }
+
+                    valid = true;
+
+                }else{
+
+                    System.out.println("'"+guess+"'"+" is not a valid input");
+
+                    valid = false;
+                }
+
+            }else{
+
+                trash = in.next();
+
+                System.out.println("'"+trash+"'"+" is not a valid input");
+
+                valid = false;
+            }
+
+        }while(!valid);
+    }
 }
